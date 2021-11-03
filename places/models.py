b88727ccf2716be82dd41dcf96c0b9faad12f9b4
canto9112 <cantoweb112@gmail.com>
2021-11-03 11:24:27 +0300

    Добавил verbose_name к моделям

--- places/models.py
+++ places/models.py
@@ -4,21 +4,20 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
 
-    title = models.CharField(max_length=200)
-    description_short = models.TextField()
-    description_long = models.TextField()
-    lng = models.FloatField()
-    lat = models.FloatField()
-    content = HTMLField(null=True)
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description_short = models.TextField(verbose_name='Короткое описание')
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
+    content = HTMLField(null=True, verbose_name='Содержание')
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(verbose_name='Картинка')
-    img_number = models.IntegerField()
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='Место')
+    image = models.ImageField(verbose_name='Изображение')
+    img_number = models.IntegerField(verbose_name='Номер изображения ')
 
     def __str__(self):
         return f'{self.img_number} {str(self.place)}'
