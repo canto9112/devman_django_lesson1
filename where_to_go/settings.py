b43c015f54aa88f1d1a164a225b6f38349407f39
canto9112 <cantoweb112@gmail.com>
2021-11-03 13:53:18 +0300

    Изменил ALLOWED_HOSTS

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = env.str('SECRET_KEY', default='django-insecure-c*7aetmrp#)m)ben$48f
 
 DEBUG = env.bool('DEBUG',  default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'canto911.pythonanywhere.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', 'canto911.pythonanywhere.com'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
