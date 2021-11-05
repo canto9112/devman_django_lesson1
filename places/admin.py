c9586fce2f8005353528ddcfdd05b47dcdd47435
canto9112 <cantoweb112@gmail.com>
2021-11-05 16:48:19 +0300

    Заменил регистрацию модели на декоратор

--- places/admin.py
+++ places/admin.py
@@ -5,19 +5,12 @@ from django.utils.html import format_html
 from .models import Image, Place
 
 
-class ImageInstanceInline(SortableInlineAdminMixin, admin.TabularInline):
-    model = Image
-    readonly_fields = ["preview"]
-
-    def preview(self, obj):
-        return format_html("<img src='{}' height=200px />", obj.image.url)
-
-
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInstanceInline]
 
 
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     raw_id_fields = ['place']
     readonly_fields = ["preview"]
@@ -26,5 +19,10 @@ class ImageAdmin(admin.ModelAdmin):
         return format_html("<img src='{}' height=200px />", obj.image.url)
 
 
-admin.site.register(Image, ImageAdmin)
+class ImageInstanceInline(SortableInlineAdminMixin, admin.TabularInline):
+    model = Image
+    readonly_fields = ["preview"]
+
+    def preview(self, obj):
+        return format_html("<img src='{}' height=200px />", obj.image.url)
 
