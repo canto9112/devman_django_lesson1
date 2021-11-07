9ef34cbee9e41337ca871c33886558ae390b1080
canto9112 <cantoweb112@gmail.com>
2021-11-07 21:18:56 +0300

    удалил null=True

--- places/models.py
+++ places/models.py
@@ -5,16 +5,12 @@ from tinymce.models import HTMLField
 class Place(models.Model):
 
     title = models.CharField(max_length=200,
-                             verbose_name='Заголовок',
-                             null=True)
+                             verbose_name='Заголовок')
     description_short = models.TextField(verbose_name='Короткое описание',
-                                         null=True,
                                          blank=True)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
-    content = HTMLField(null=True,
-                        verbose_name='Содержание',
-                        blank=True)
+    content = HTMLField(verbose_name='Содержание', blank=True)
 
     def __str__(self):
         return self.title
